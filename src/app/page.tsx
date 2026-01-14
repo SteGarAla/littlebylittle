917d625b5989cf1e6d661fa770c65ed29df14060
garala <StevenGarcia.cs@gmail.com>
2026-01-14 13:07:10 -0600

    user is able to turn on and off camera. UI needs to be worked on.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function Home() {
-  return <div> littlebylittle-book collection</div>;
+  // video is of type HTMLVideoElement or null
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  async function startCamera() {
+    // returns mediaStream object if successful (only using users camera no audio being captured)
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+
+    // checking if we have anything running and make that the current
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+      await videoRef.current.play();
+    }
+  }
+
+  // grassy.matcha.darkchocolate.its weird because the flavor sticks to your tongue and its like matcha
+  async function stopCamera() {
+    const video = videoRef.current;
+    if (!video) {
+      return;
+    }
+
+    const stream = video.srcObject as MediaStream | null;
+
+    stream?.getTracks().forEach((track) => track.stop());
+    video.srcObject = null;
+  }
+
+  return (
+    <>
+      <div> littlebylittle - book collection</div>
+      <video ref={videoRef} autoPlay playsInline />
+      <button type="button" onClick={startCamera}>
+        {" "}
+        start the camera
+      </button>
+      <button type="button" onClick={stopCamera}>
+        {" "}
+        stop the camera
+      </button>
+    </>
+  );
 }
